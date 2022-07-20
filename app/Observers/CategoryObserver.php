9d826846d0508082a69831075b417d8d5405d478
Eric <>
2022-07-20 17:48:39 -0400

    Adding observers

---
+++ app/Observers/CategoryObserver.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Budget;
+use App\Models\Category;
+use Illuminate\Support\Facades\Auth;
+
+class CategoryObserver
+{
+    /**
+     * Handle to the post "creating" event.
+     *
+     * @param  \App\Models\Account  $account
+     * @return void
+     */
+    public function creating(Category $category)
+    {
+        $category->user_id = Auth::user()->id;
+    }
+
+    /**
+     * Handle the Category "created" event.
+     *
+     * @param  \App\Models\Category  $category
+     * @return void
+     */
+    public function created(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Handle the Category "updated" event.
+     *
+     * @param  \App\Models\Category  $category
+     * @return void
+     */
+    public function updated(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Handle the Category "deleted" event.
+     *
+     * @param  \App\Models\Category  $category
+     * @return void
+     */
+    public function deleted(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Handle the Category "restored" event.
+     *
+     * @param  \App\Models\Category  $category
+     * @return void
+     */
+    public function restored(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Handle the Category "force deleted" event.
+     *
+     * @param  \App\Models\Category  $category
+     * @return void
+     */
+    public function forceDeleted(Category $category)
+    {
+        //
+    }
+}
