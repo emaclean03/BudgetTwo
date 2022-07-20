53b0b71a7cf6eae1ef11cc32eeac9b342b8be608
Eric <>
2022-07-20 13:53:53 -0400

    Adding new account

--- resources/js/Pages/Account/AccountCreate.tsx
+++ resources/js/Pages/Account/AccountCreate.tsx
@@ -6,15 +6,15 @@ import {AccountReducer} from "../../reducers/Account/accountReducer";
 import {Inertia} from "@inertiajs/inertia";
 
 
-
 const AccountCreate = () => {
     const handleCreateAccount = () => {
         const newAccountPrompt = prompt('What is your new account name?');
-        Inertia.post('/account/store', {account_name: newAccountPrompt, budget_id: 1, account_type: 'checking', account_balance: 0.00})
+        //TODO: Updated budget_id with redis?
+        //TODO: Figure out a way to add account_type and account_balance
+        Inertia.post('/account/store',
+            {account_name: newAccountPrompt, budget_id: 1, account_type: 'checking', account_balance: 0.00})
     }
-
-
-    return(
+    return (
         <button className={'text-sm font-semibold text-blue-600'} onClick={handleCreateAccount}>+ New Account</button>
     )
 }
