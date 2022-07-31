752e4bf6914a12fdd0fc89e5f35afc1a5275fe57
Eric <>
2022-07-31 09:43:43 -0400

    Adding modal for new category

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,7 +2,8 @@
 <html>
 <head>
     <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <meta name="viewport" content="initial-scale=1, width=device-width" />
+
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
     <script src="{{ mix('/js/app.js') }}" defer></script>
 </head>
